d2d23dc5594f77b60b6b4105768e26657c395498
Jennie Zeng <jenniezeng1@gmail.com>
2019-05-29 11:45:00 -0700

    Functional Stack method completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,23 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var keys = Object.keys(storage);
+    var keysCount = keys.length;
+
+    storage[keysCount] = value;
   };
 
   someInstance.dequeue = function() {
+    var originalFirst = storage[0];
+
+
+
+    return originalFirst;
   };
 
   someInstance.size = function() {
+    var keys = Object.keys(storage);
+    return keys.length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,27 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //get current length of keys in storage to see how many items are in storage
+    var keys = Object.keys(storage);
+    //get the total item counts in storage
+    var itemCount = keys.length;
+
+    storage[itemCount] = value;
   };
 
   someInstance.pop = function() {
+    //get current length of keys in storage to see how many items are in storage
+    var keys = Object.keys(storage);
+    //get the total item counts in storage
+    var itemCount = keys.length;
+    var toBeRemoved = storage[itemCount - 1];
+    delete storage[itemCount - 1];
+    return toBeRemoved;
   };
 
   someInstance.size = function() {
+    var keys = Object.keys(storage);
+    return keys.length;
   };
 
   return someInstance;
