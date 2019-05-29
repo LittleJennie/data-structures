00ba85bc5311e93e6e0b954f90559b793dee427f
Jennie Zeng <jenniezeng1@gmail.com>
2019-05-29 15:08:57 -0700

    ES6 stack and queue methods completed

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,35 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+    this.storage = {};
+  };
 
+  enqueue(value) {
+    var self = this.storage;
+
+    var itemCount = (Object.keys(self)).length;
+    self[itemCount] = value;
+  };
+
+  dequeue() {
+    var self = this.storage;
+
+    var firstItem = self[0];
+    delete self[0];
+
+    for(var prop in self) {
+      self[prop - 1] = self[prop];
+      if (!self[prop + 1]) {
+        delete self[prop];
+      }
+    };
+
+    return firstItem;
+  };
+
+  size(){
+    var self = this.storage;
+
+    return (Object.keys(self)).length;
+  };
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    var self = this.storage;
+    var itemCount = (Object.keys(self)).length;
+
+    self[itemCount] = value;
+  };
+
+  pop() {
+    var self = this.storage;
+    var itemCount = (Object.keys(self)).length;
+    var lastItem = self[itemCount - 1];
+
+    delete self[itemCount - 1];
+
+    return lastItem;
+  };
+
+  size() {
+    return (Object.keys(this.storage)).length;
+  };
 }
