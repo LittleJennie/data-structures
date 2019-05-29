152a46d039bd7a1ef5444685fbbc30a3d6f77609
Jennie Zeng <jenniezeng1@gmail.com>
2019-05-29 12:12:25 -0700

    Functional shared structure set up

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  var storage = {};
+
+  extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function() {
+
+  }, 
+  
+  pop: function() {
+
+  }, 
+
+  size: function() {
+
+  }
+
+};
 
 
